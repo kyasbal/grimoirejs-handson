0a4ff1ba31d2696077845b0df99ec9aa7b48be75
case2912 <shiroyama@maia.eonet.ne.jp>
2017-01-28 11:07:20 +0900

    fix: Session2

--- Session2/02.js
+++ Session2/02.js
@@ -1,62 +1,24 @@
 const Quaternion = gr.lib.math.Quaternion;
-gr(function() {
-    const scene = gr("#main")("scene").single();
-    setInterval(function() {
-        const n = scene.addChildByName("rigid-sphere", {
-            position: [Math.random() * 2 - 1, 6, 0],
-            targetBuffer:"wireframe"
-        });
-        // n.on("mouseenter", function() {
-        //   n.setAttribute("color","blue");
-        // });
-    }, 1000);
-});
-gr.register(() => {
-    gr.registerComponent("OimoScene", {
-        $awake: function() {
-            this.world = new OIMO.World();
-        },
-        $update: function() {
-            this.world.step();
-        }
-    });
-    gr.overrideDeclaration("scene", ["OimoScene"]);
-    gr.registerComponent("Rigid", {
+gr.register(function() {
+    gr.registerComponent("Rotate", {
         attributes: {
-            shape: {
-                default: "sphere",
-                converter: "String"
+            speed: {
+                default: 1,
+                converter: "Number"
             },
-            move: {
-                converter: "Boolean",
-                default: true
+            axis: {
+                default: [0, 1, 0],
+                converter: "Vector3"
             }
         },
-        $mount: function() {
+        $awake: function() {
             this.__bindAttributes();
-            this.transform = this.node.getComponent("Transform");
-            const p = this.transform.localPosition;
-            const r = this.transform.localRotation;
-            const s = this.transform.localScale;
-            const oimoScene = this.node.getComponentInAncesotor("OimoScene");
-            this.body = oimoScene.world.add({
-                type: this.shape,
-                size: [s.X, s.Y, s.Z],
-                pos: [p.X, p.Y, p.Z],
-                rot: [r.X, r.Y, r.Z],
-                move: this.move,
-                density: 1
-            });
+            this._transform = this.node.getComponent("Transform");
         },
+        $mount: function() {},
         $update: function() {
-            const p = this.body.getPosition();
-            this.transform.setAttribute("position", [p.x, p.y, p.z]);
-            const r = this.body.getQuaternion();
-            this.transform.setAttribute("rotation", new Quaternion([r.x, r.y, r.z, r.w]));
+            this._transform.localRotation = Quaternion.multiply(
+                Quaternion.angleAxis(this.speed * 0.01, this.axis), this._transform.localRotation);
         }
     });
-    gr.registerNode("rigid-sphere", ["Rigid"], {
-        color: "green",
-        geometry: "sphere"
-    }, "mesh");
 });
